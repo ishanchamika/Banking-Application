d98eba94b676fcd727f5940080d9d44febee9c7b
ishanchamika <ishanchami9@gmail.com>
2024-09-26 23:34:31 +0530

    developed crud operations

--- src/main/java/com/example/demoBank/controller/AccountController.java
+++ src/main/java/com/example/demoBank/controller/AccountController.java
@@ -5,10 +5,9 @@ import com.example.demoBank.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/account")
@@ -22,5 +21,40 @@ public class AccountController
     {
        Account createAccount =  service.createAccount(account);
        return ResponseEntity.status(HttpStatus.CREATED).body(createAccount);
+    }
+
+    @GetMapping("/{accountNumber}")
+    public Account getAccountByAccountNumber(@PathVariable Long accountNumber)
+    {
+        Account account = service.getAccountDetailsByAccountNumber(accountNumber);
+        return account;
+    }
+
+    @GetMapping("/getallaccounts")
+    public List<Account> getAllAccountDetauls()
+    {
+        List<Account> allAccountDetails = service.getAllAccountDetails();
+        return allAccountDetails;
+    }
+
+    @PutMapping("/deposit/{accountNumber}/{amount}")
+    public Account depositAccount(@PathVariable Long accountNumber, @PathVariable Double amount)
+    {
+        Account account = service.depositAmount(accountNumber, amount);
+        return account;
+    }
+
+    @PutMapping("/withdrawa/{accountNumber}/{amount}")
+    public Account withdrawaAccount(@PathVariable Long accountNumber, @PathVariable Double amount)
+    {
+        Account account = service.withdrawaAmount(accountNumber, amount);
+        return  account;
+    }
+
+    @DeleteMapping("delete/{accountNumber}")
+    public ResponseEntity deleteAccount(@PathVariable Long accountNumber)
+    {
+        service.closeAccount(accountNumber);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Account closed");
     }
 }
--- src/main/java/com/example/demoBank/service/AccountServiceImpl.java
+++ src/main/java/com/example/demoBank/service/AccountServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccountServiceImpl implements AccountService
@@ -20,27 +21,68 @@ public class AccountServiceImpl implements AccountService
     }
 
     @Override
-    public Account getAccountDetailsByAccountNumber(Long accountNumber) {
-        return null;
+    public Account getAccountDetailsByAccountNumber(Long accountNumber)
+    {
+        Optional<Account> account = repo.findById(accountNumber);
+        if(account.isEmpty())
+        {
+            throw new RuntimeException("Account is not present");
+        }
+        else
+        {
+            Account account_found = account.get();
+            return account_found;
+        }
     }
 
     @Override
     public List<Account> getAllAccountDetails() {
-        return List.of();
+        List<Account> listOfAccounts = repo.findAll();
+        return listOfAccounts;
     }
 
     @Override
     public Account depositAmount(Long accountNumber, Double amount) {
-        return null;
+        Optional<Account> account = repo.findById(accountNumber);
+
+        if(account.isEmpty())
+        {
+            throw new RuntimeException("Account is not present");
+        }
+        else
+        {
+            Account accountPresent = account.get();
+            Double totalBalance =  accountPresent.getAccount_balance() + amount;
+            accountPresent.setAccount_balance(totalBalance);
+            repo.save(accountPresent);
+            return accountPresent;
+        }
     }
 
     @Override
-    public Account withdrawaAmount(Long accountNumber, Double amount) {
-        return null;
+    public Account withdrawaAmount(Long accountNumber, Double amount)
+    {
+        Optional<Account> account = repo.findById(accountNumber);
+
+        if(account.isEmpty())
+        {
+            throw new RuntimeException("Account is not present");
+        }
+        else
+        {
+            Account accountPresent = account.get();
+            Double accountBalance =  accountPresent.getAccount_balance()-amount;
+            accountPresent.setAccount_balance(accountBalance);
+            repo.save(accountPresent);
+
+            return accountPresent;
+        }
     }
 
     @Override
-    public void closeAccount(Long accountNumber) {
-
+    public void closeAccount(Long accountNumber)
+    {
+        getAccountDetailsByAccountNumber(accountNumber);
+        repo.deleteById(accountNumber);
     }
 }
